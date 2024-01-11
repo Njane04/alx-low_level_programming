a8e328fe51aaf21897e528ec722c9cab012c252a
Njane04 <@loisenjane02>
2024-01-11 13:03:36 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area
+ * @dest: memory where stored
+ * @src: memory where copied
+ *  *@n: number of bytes
+ *
+ *  Rerurn: copied memory with n byted changed
+*/
+
+char *_memcpy(char *dest, char *src, usigned int n)
+{
+	int r = o;
+	int i = n :
+
+	for (; r, i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
+
