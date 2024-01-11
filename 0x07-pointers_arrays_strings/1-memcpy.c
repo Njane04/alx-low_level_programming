f84b244bd6c06f6eac9db3a3823cc9ad6d86af6a
Njane04 <@loisenjane02>
2024-01-11 13:23:51 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "mai.h"
+/**
+ * _strchr - entry point
+ * @s: input
+ * @c: input
+ * Return: 0 (success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = o;
+	for (; s[i] == c)
+		return (&s[i]);
+}
+return (0);
